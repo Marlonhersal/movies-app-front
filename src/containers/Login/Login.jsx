ab896b4a0bf2740a7de4559f5179f0b1eed04fea
MarlonHersal <hern.sal.dz@gmail.com>
2022-12-28 12:05:54 -0600

    Validacion de formularios

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,54 +1,82 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import Style from './Login.module.scss'
-import {Link, Navigate} from "react-router-dom";
-import { data } from 'autoprefixer';
-
-
+import Style from "./Login.module.scss";
+import { Link, Navigate } from "react-router-dom";
+import { data } from "autoprefixer";
 
 function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!/\S+@\S+\.\S+/.test(email)) return setMessage("Email requerido");
+    if (!password) return setMessage("Contraseña requerida");
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // enviar la solicitud POST a la API
-        fetch('http://localhost:3000/auth/login', {
-          method: 'POST',
-          body: JSON.stringify({email, password}),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-        .then(res => res.json())
-        .then(res => {
-        const {message, token} = res;
+    fetch("http://localhost:3000/auth/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        const { message, token } = res;
+        if (!token) {
+          return setMessage("Las credenciales no son validas");
+        }
         localStorage.setItem("Token", token);
-        window.location.href = '/browse';
-        })
-        
-        .catch((error) => {
-          console.log( error)
-        });
-      };
+        window.location.href = "/browse";
+      })
+      .catch(function (error) {
+        console.log("Hubo un problema con la petición Fetch:" + error.message);
+      });
+  };
 
-    return (
+  return (
     <div className={Style.login}>
-        <div className={Style.form_container}>
-            <span className={Style.logo}></span>
-            <form action="/" className={Style.form} onSubmit={handleSubmit}  >
-            <label for="email" className={Style.label}>Email address</label>
-            <input type="text" id="email" placeholder="example@mail.cm" className={`${Style.input} ${Style.input_email}`} onChange={(event) => setEmail(event.target.value)} ></input>
-            <label for="password" className={Style.label}>Password</label>
-            <input type="password" id="password" placeholder="*********" className={`${Style.input} ${Style.input_password}`} onChange={(event) => setPassword(event.target.value)}></input>
-            <Link to='/browse'>Forgot my password</Link>
-            <input type="submit" value="Log in" className={`${Style.primary_button} ${Style.login_button}`}/>
-            <Link to='/register' className={`${Style.secondary_button}  ${Style.singup_button}`}><span>Sing up</span></Link>
-            </form>
-        </div>
+      <div className={Style.form_container}>
+        <span className={Style.logo}></span>
+        <form action="/" className={Style.form} onSubmit={handleSubmit}>
+          <label className={Style.message}>{message}</label>
+          <label for="email" className={Style.label}>
+            Email address
+          </label>
+          <input
+            type="text"
+            id="email"
+            placeholder="example@mail.cm"
+            className={`${Style.input} ${Style.input_email}`}
+            onChange={(event) => setEmail(event.target.value)}
+          ></input>
+          <label for="password" className={Style.label}>
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="*********"
+            className={`${Style.input} ${Style.input_password}`}
+            onChange={(event) => setPassword(event.target.value)}
+          ></input>
+          <Link to="/browse">Forgot my password</Link>
+          <input
+            type="submit"
+            value="Log in"
+            className={`${Style.primary_button} ${Style.login_button}`}
+          />
+          <Link
+            to="/register"
+            className={`${Style.secondary_button}  ${Style.singup_button}`}
+          >
+            <span>Sing up</span>
+          </Link>
+        </form>
+      </div>
     </div>
-    );
+  );
 }
 
 export default Login;
--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,54 +1,88 @@
-import { Alert } from 'bootstrap';
-import React, {useState} from 'react';
-import { Link, redirect } from 'react-router-dom';
+import { Alert } from "bootstrap";
+import React, { useState } from "react";
+import { Link, redirect } from "react-router-dom";
 
-import Style from './Register.module.scss'
+import Style from "./Register.module.scss";
 
 function Register(props) {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        // enviar la solicitud POST a la API
-        fetch('http://localhost:3000/users/', {
-          method: 'POST',
-          body: JSON.stringify({ name, email, password, role:'customer'}),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-                console.log(data)
-                alert('Cuenta Creada')
-                window.location.replace('/login');
-            
-          })
-          .catch((error) => {
-            // manejar el error
-          });
-      };
-    return (
-        <div className={Style.login} onSubmit={handleSubmit}>
-            <div className={Style.form_container}>
-                <span className={Style.logo}></span>
-                <h1 className={Style.title}>My account</h1>
-                <form action="/" className={Style.form}>
-                    <div>
-                        <label for="name" className={Style.label}>Name</label>
-                        <input type="text" id="name" placeholder="Camila Yoko" className={`${Style.input} ${Style.input_name}`} onChange={(event) => setName(event.target.value)}></input>
-                        <label for="email-address" className={Style.label}>Email address</label>
-                        <input type="email" id="email-address" placeholder="camilayokoo@gmail.com" className={`${Style.input} ${Style.input_email}`} onChange={(event) => setEmail(event.target.value)}></input>
-                        <label for="password" className={Style.label}>Password</label>
-                        <input type="password" id="password" placeholder="*******" className={`${Style.input} ${Style.input_password}`} onChange={(event) => setPassword(event.target.value)}></input>
-                    </div>
-                    <input type="submit" value="Create" className= {`${Style.primary_button} ${Style.create_button}`}></input>
-                </form>
-            </div>
-        </div>
-    );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!name) return setMessage("Nombre requerido");
+    if (!/\S+@\S+\.\S+/.test(email)) return setMessage("Email requerido");
+    if (!password) return setMessage("Contraseña requerida");
+
+    fetch("http://localhost:3000/users/", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password, role: "customer" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.statusCode === 400) {
+          return setMessage(data.message);
+        }
+        alert("Cuenta Creada");
+        window.location.replace("/login");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div className={Style.login} onSubmit={handleSubmit}>
+      <div className={Style.form_container}>
+        <span className={Style.logo}></span>
+        <h1 className={Style.title}>My account</h1>
+        <form action="/" className={Style.form}>
+          <label className={Style.message}>{message}</label>
+          <div>
+            <label for="name" className={Style.label}>
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="Camila Yoko"
+              className={`${Style.input} ${Style.input_name}`}
+              onChange={(event) => setName(event.target.value)}
+            ></input>
+            <label for="email-address" className={Style.label}>
+              Email address
+            </label>
+            <input
+              type="email"
+              id="email-address"
+              placeholder="camilayokoo@gmail.com"
+              className={`${Style.input} ${Style.input_email}`}
+              onChange={(event) => setEmail(event.target.value)}
+            ></input>
+            <label for="password" className={Style.label}>
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="*******"
+              className={`${Style.input} ${Style.input_password}`}
+              onChange={(event) => setPassword(event.target.value)}
+            ></input>
+          </div>
+          <input
+            type="submit"
+            value="Create"
+            className={`${Style.primary_button} ${Style.create_button}`}
+          ></input>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
