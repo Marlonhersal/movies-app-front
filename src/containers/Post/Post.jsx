694d35bf56c00bcab9a4d6f674392195241b081c
MarlonHersal <hern.sal.dz@gmail.com>
2023-01-02 16:04:38 -0600

    Post

--- src/containers/Post/Post.jsx
+++ src/containers/Post/Post.jsx
@@ -1,30 +1,18 @@
 import React, { useState, useEffect } from "react";
 import S from "./Post.module.scss";
 import { useSelector } from "react-redux";
-function Post(props) {
-  const redux = useSelector(state => state.itemLoaded)
-  const [datos, setDatos] = useState({});
-  console.log("Contructor")
-  useEffect(() => {
-    console.log("Ahora nos volvimo a ilusionar")
-    setDatos(redux)
-    console.log(datos)
-  },[redux]);
 
-  /* const getData = async () => {
-    const data = await fetch(
-      `http://localhost:3000/${props.element}/${props.id}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + props.token,
-        },
-      }
-    );
-    const dataJson = await data.json();
-    setDatos(dataJson);
-  }; */
+function Post(props) {
+  const redux = useSelector((state) => state.itemLoaded);
+  const [datos, setDatos] = useState({});
+  const [newItem, setNewItem] = useState(false);
+  const [uptdate, setUpdate] = useState(true);
+
+  const notUptate = ["updatedAt", "createdAt", "id", "age", "movie","actors", "movies"];
+
+  useEffect(() => {
+    setDatos(redux);
+  }, [redux]);
 
   const handleDataChange = (e) => {
     const { name, value } = e.target;
@@ -32,17 +20,15 @@ function Post(props) {
       ...datos,
       [name]: value,
     });
+    setUpdate(false)
   };
 
-  const notUptate = ["updatedAt", "createdAt", "id", "age", "role", "movies"]
-
- const actualizar = (event) => {
+  const actualizar = (event) => {
     event.preventDefault();
     let info = datos;
-    notUptate.forEach((x)=>{
-      delete info[x]
-    })
-
+    notUptate.forEach((x) => {
+      delete info[x];
+    });
     fetch(`http://localhost:3000/${props.element}/${props.id}`, {
       method: "PATCH",
       body: JSON.stringify(info),
@@ -53,17 +39,20 @@ function Post(props) {
     })
       .then((res) => res.json())
       .then((res) => {
-  
         if (!res) {
           return setMessage("Las credenciales no son validas");
         }
-        console.log(res)
+        console.log(res);
+        setUpdate(true)
       })
       .catch(function (error) {
         console.log("Hubo un problema con la petición Fetch:" + error.message);
       });
+
   };
-  const borrar =()=> {
+
+  const borrar = () => {
+    console.log(props.element, props.id)
     fetch(`http://localhost:3000/${props.element}/${props.id}`, {
       method: "DELETE",
       headers: {
@@ -73,29 +62,30 @@ function Post(props) {
     })
       .then((res) => res.json())
       .then((res) => {
-        alert(props.element + " " + res + "Borrado")
+        console.log(res)
       })
       .catch(function (error) {
         console.log("Hubo un problema con la petición Fetch:" + error.message);
       });
-  }
-  const create = ()=>{
-    let datosVacios= {}
+  };
+
+  const create = () => {
+    let datosVacios = {};
     Object.keys(redux).forEach((key, i) => {
       datosVacios = {
         ...datosVacios,
-      [key]: "",
-      }
-    })
-    
-    setDatos(datosVacios)
-  }
+        [key]: "",
+      };
+    });
+    setDatos(datosVacios);
+    setNewItem(true)
+  };
 
-  const publicar = ()=>{
+  const publicar = () => {
     let info = datos;
-    notUptate.forEach((x)=>{
-      delete info[x]
-    })
+    notUptate.forEach((x) => {
+      delete info[x];
+    });
     fetch(`http://localhost:3000/${props.element}`, {
       method: "POST",
       body: JSON.stringify(info),
@@ -106,40 +96,87 @@ function Post(props) {
     })
       .then((res) => res.json())
       .then((res) => {
-  
         if (!res) {
           return setMessage("Las credenciales no son validas");
         }
-        console.log(res)
+        console.log(res);
       })
       .catch(function (error) {
         console.log("Hubo un problema con la petición Fetch:" + error.message);
       });
-  }
-  
-  
+  };
+
+  const agregarActor = (e) => {
+    e.preventDefault();
+    const actorId = document.getElementById("idActor").value;
+    const movieId = datos.id;
+    fetch(`http://localhost:3000/movies/add-actor`, {
+      method: "POST",
+      body: JSON.stringify({ movieId, actorId }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + props.token,
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(function (error) {
+        console.log("Hubo un problema con la petición Fetch:" + error.message);
+      });
+  };
+
   return (
-    <form className={S.container} >
+    <form className={S.container}>
       {Object.keys(datos).map((key, i) => (
         <div key={`input-${i}`}>
           <label htmlFor={`key`}>{key}</label>
-          { 
-            !notUptate.includes(key)?<input
-            type="text"
-            id={key}
-            name={key}
-            value={datos[key]}
-            onChange={handleDataChange}
-          />: <span>{datos[key]}</span>
-          }
-          
+          {props.element == "user" ? (
+            <span>{datos[key]}</span>
+          ) : !notUptate.includes(key) ? (
+            <input
+              type="text"
+              id={key}
+              name={key}
+              value={datos[key]}
+              onChange={handleDataChange}
+            />
+          ) : (
+            <span>
+              {typeof datos[key] != "object"
+                ? datos[key]
+                : datos[key].map((x) => (
+                    <div>
+                      <span>{x.id}</span>
+                      <span>{x.name}</span>
+                    </div>
+                  ))}
+            </span>
+          )}
         </div>
       ))}
-       <input type="button" value="mostrar" onClick={()=> console.log(datos)}/>
-       <input type="button" value="borrrar" onClick={borrar}/>
-       <input type="button" value={"Nuevo"} onClick={create}/>
-       <input type="button" value="Actualizar" onClick={actualizar}/>
-       <input type="button" value="Crear" onClick={publicar}/>
+
+      {
+        props.element == "movies"?<div >
+        <label>New {props.element}</label>
+        <input type="number" placeholder="actor id" id="idActor" />
+        <input type="button" value="Agregar" onClick={agregarActor} />
+        </div>:
+        null
+      }
+      
+
+      <input type="button" value="mostrar" onClick={() => console.log(datos)} disabled= "true"/>
+
+      <input type="button" value="Actualizar"  onClick={actualizar} disabled={uptdate || newItem} />
+      <input type="button" value="borrrar" id="delete_button" onClick={borrar} disabled={newItem}/>
+      {
+        newItem? <input type="button" value="Crear" onClick={publicar}/>:
+        <input type="button" value="Nuevo" id="create_button" onClick={create}/>
+      }
+      
+      
     </form>
   );
 }
